986eaec80fb315cc89d4a5555eab722d8e6d7294
Greg Roggero <gregorio.roggero@gmail.com>
2021-10-18 16:21:24 -0500

    commit project files to github

---
+++ script.js
@@ -0,0 +1,32 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Write your code below:
+
+const generateTarget = () => {
+return Math.floor(Math.random() * 10);
+};
+
+const compareGuesses = (humanGuess, computerGuess, targetGuess) => {
+    const humanDifference = Math.abs(humanGuess - targetGuess);
+    const computerDifference = Math.abs(computerGuess - targetGuess);
+    
+    if(humanDifference <= computerDifference) {
+      return true;
+    } else {
+      return false;
+    }
+};
+
+const updateScore = theWinner => {
+    if (theWinner === 'human') {
+        humanScore++;
+    } else if (theWinner === 'computer') {
+        computerScore++;
+    }
+};
+
+const advanceRound = () => currentRoundNumber++;
+
+
